a4d209cda144f125c8ae0e291d8e36a3968c6a1a
Jonghyeok <jhpmuscle@gmail.com>
2024-07-22 16:51:24 +0900

    style: manage page layout

--- src/routes/Manage.jsx
+++ src/routes/Manage.jsx
@@ -1,7 +1,40 @@
+import { Link } from 'react-router-dom';
+import './Manage.css';
+import arrowIcon from '../assets/icons/arrow-icon.png';
+import searchIcon from '../assets/icons/search-icon.png';
+
 function Manage() {
   return (
     <main className="manage">
-      
+      <section className='manage-search-wrapper'>
+        <div className='search-input'>
+          <input type="text" name="search" id="search" placeholder='검색...' />
+          <button className='search-input-btn'>
+            <img src={searchIcon} alt="search-icon" />
+          </button>
+        </div>
+      </section>
+      <section className='manage-user-list'>
+        <table className='manage-user-list-table'>
+          <tbody>
+            <tr className='manage-user-list-item'>
+              <td>1</td>
+              <td>2022123455</td>
+              <td>
+                <button className='manage-user-list-redirect-btn'>챗봇과의 대화기록 보기</button>
+              </td>
+              <td>
+                <button className='manage-user-list-redirect-btn'>나와의 대화기록 보기</button>
+              </td>
+              <td>
+                <Link>
+                  자세히 보기 <img src={arrowIcon} alt="arrow-icon" />
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </main>
   )
 }
