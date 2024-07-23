5b095a7707d7bcd610956c56a4a58f56b7ed2dbd
Jonghyeok <jhpmuscle@gmail.com>
2024-07-23 16:47:44 +0900

    feat: get user list api

---
+++ src/api/userApi.js
@@ -0,0 +1,10 @@
+const getUserList = async (page, pageSize) => {
+  let res = await fetch(import.meta.env.VITE_APP_API_URL + `/api/user/list?page=${page}&pageSize=${pageSize}`)
+  
+  if (!res.ok) {
+    let message = await res.json();
+    throw new Error(message);
+  }
+
+  return res.json();
+}
