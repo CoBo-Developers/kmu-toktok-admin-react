8eaa11bd4e0c662cda0c4770954e4f016bd90403
Jonghyeok <jhpmuscle@gmail.com>
2024-07-31 15:28:03 +0900

    feat: disable input if not modifying

--- src/components/aside/ManageMenu/ManageMenu.jsx
+++ src/components/aside/ManageMenu/ManageMenu.jsx
@@ -6,9 +6,20 @@ function ManageMenu() {
   const selectedUser = useSeletectUserStore((state) => state.seletedUser);
   const [selectedRole, setSelectedRole] = useState();
   const [inputId, setInputId] = useState();
+  const [isModify, setIsModify] = useState(false);
 
   const handleRoleChange = (e) => {
     setSelectedRole(e.target.value);
+  }
+
+  const handleStudentIdChange = (e) => {
+    setInputId(e.target.value);
+  }
+
+  const handleClickModifyBtn = () => {
+    // if (!isModify) {
+      setIsModify(!isModify);
+    // }
   }
 
   useEffect(() => {
@@ -24,26 +35,26 @@ function ManageMenu() {
         <div className="aside-user-manage-ratio-container">
           <div className="aside-user-manage-ratio">
             <input type="radio" id="student" name="role" value="STUDENT" checked={selectedRole == "STUDENT" ? true : false} 
-              onChange={handleRoleChange} />
+              onChange={handleRoleChange} disabled={isModify ? false : true} />
             <label htmlFor="student">학생</label>
           </div>
           <div className="aside-user-manage-ratio">
             <input type="radio" id="professor" name="role" value="PROFESSOR" checked={selectedRole == "PROFESSOR" ? true : false}
-              onChange={handleRoleChange} />
+              onChange={handleRoleChange} disabled={isModify ? false : true} />
             <label htmlFor="professor">교수자</label>
           </div>
           <div className="aside-user-manage-ratio">
             <input type="radio" id="developer" name="role" value="DEVELOPER" checked={selectedRole == "DEVELOPER" ? true : false}
-              onChange={handleRoleChange} />
+              onChange={handleRoleChange} disabled={isModify ? false : true} />
             <label htmlFor="developer">관리자</label>
           </div>
         </div>
       </div>
       <div className='aside-user-manage-id-input'>
         <label htmlFor="student-id">학번</label>
-        <input type="text" name="student-id" id='student-id' value={inputId} disabled />
+        <input type="text" name="student-id" id='student-id' value={inputId} onChange={handleStudentIdChange} disabled={isModify ? false : true} />
       </div>
-      <button className='aside-user-manage-modify-btn'>정보 수정</button>
+      <button className={'aside-user-manage-modify-btn ' + (isModify ? "active" : null)} onClick={handleClickModifyBtn}>정보 수정</button>
       <button className='aside-user-redirect-btn'>챗봇과의 대화기록 보기</button>
       <button className='aside-user-redirect-btn'>나와의 대화기록 보기</button>
     </section>
