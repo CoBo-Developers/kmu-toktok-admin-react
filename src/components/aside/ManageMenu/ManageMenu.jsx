dd21b1f7609c2852ac5c69151a3c8182c7613ca1
Jonghyeok <jhpmuscle@gmail.com>
2024-07-31 15:17:01 +0900

    feat: set changing info with initial selected user info

--- src/components/aside/ManageMenu/ManageMenu.jsx
+++ src/components/aside/ManageMenu/ManageMenu.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
 import useSeletectUserStore from "../../../store/useSeletedUserStore";
 import './ManageMenu.css';
 
 function ManageMenu() {
   const selectedUser = useSeletectUserStore((state) => state.seletedUser);
+  const [selectedRole, setSelectedRole] = useState();
+  const [inputId, setInputId] = useState();
+
+  const handleRoleChange = (e) => {
+    setSelectedRole(e.target.value);
+  }
+
+  useEffect(() => {
+    setSelectedRole(selectedUser.role);
+    setInputId(selectedUser.studentId);
+  }, [selectedUser])
 
   return (
     <section className='aside-user-manage'>
@@ -11,22 +23,25 @@ function ManageMenu() {
         <h5 className="aside-user-manage-input-title">역할</h5>
         <div className="aside-user-manage-ratio-container">
           <div className="aside-user-manage-ratio">
-            <input type="radio" id="student" name="role" value="STUDENT" disabled checked />
+            <input type="radio" id="student" name="role" value="STUDENT" checked={selectedRole == "STUDENT" ? true : false} 
+              onChange={handleRoleChange} />
             <label htmlFor="student">학생</label>
           </div>
           <div className="aside-user-manage-ratio">
-            <input type="radio" id="professor" name="role" value="PROFESSOR" disabled />
+            <input type="radio" id="professor" name="role" value="PROFESSOR" checked={selectedRole == "PROFESSOR" ? true : false}
+              onChange={handleRoleChange} />
             <label htmlFor="professor">교수자</label>
           </div>
           <div className="aside-user-manage-ratio">
-            <input type="radio" id="developer" name="role" value="DEVELOPER" disabled />
+            <input type="radio" id="developer" name="role" value="DEVELOPER" checked={selectedRole == "DEVELOPER" ? true : false}
+              onChange={handleRoleChange} />
             <label htmlFor="developer">관리자</label>
           </div>
         </div>
       </div>
       <div className='aside-user-manage-id-input'>
         <label htmlFor="student-id">학번</label>
-        <input type="text" name="student-id" id='student-id' value={selectedUser.studentId} disabled />
+        <input type="text" name="student-id" id='student-id' value={inputId} disabled />
       </div>
       <button className='aside-user-manage-modify-btn'>정보 수정</button>
       <button className='aside-user-redirect-btn'>챗봇과의 대화기록 보기</button>
