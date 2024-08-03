c90c4c840d9b8bbe290550565f8280baf38c7a43
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-08-04 01:37:20 +0900

    feat: chatStu api

---
+++ src/api/chatStuApi.js
@@ -0,0 +1,54 @@
+const getChat= async (accessToken, studentId) => {
+    const res = await fetch(`${import.meta.env.VITE_APP_CHAT_API_URL}/api/prof?studentId=${studentId}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  
+    if (!res.ok) {
+      const message = await res.json();
+      throw new Error(message);
+    }
+  
+    return res.json();
+};
+
+const postChat = async (accessToken, studentId, comment) => {
+    const res = await fetch(`${import.meta.env.VITE_APP_CHAT_API_URL}/api/prof`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        studentId: studentId,
+        comment: comment,
+      }),
+    });
+  
+    if (!res.ok) {
+      const message = await res.json();
+      throw new Error(message);
+    }
+  
+    return res.json();
+}
+
+const getChatList = async (accessToken, page, pageSize) => {
+    const res = await fetch(`${import.meta.env.VITE_APP_CHAT_API_URL}/api/prof/list?page=${page}&pageSize=${pageSize}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!res.ok) {
+        const message = await res.json();
+        throw new Error(message);
+      }
+    
+      return res.json();
+  }
+
+export { getChat, postChat, getChatList };
