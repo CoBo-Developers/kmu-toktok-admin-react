f6ddeae0b9d4f9cb5f93edbd50a930f50ae29e86
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-08-08 01:56:07 +0900

    fix: resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Login from './routes/Login';
 import Redirect from './routes/Redirect';
 import Register from './routes/Register';
 import useReissue from './hooks/useReissue';
+import Chatbot from './routes/Chatbot';
+import History from './routes/History';
 import ChatStu from './routes/ChatStu';
 
 function App() {
@@ -21,11 +23,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path='/chatbot' element={""} />
+        <Route path='/chatbot' element={<Chatbot />} />
         <Route path='/chatstu' element={<ChatStu />} />
         <Route path='/file' element={""} />
         <Route path='/writing' element={""} />
         <Route path='/manage' element={<Manage />} />
+        <Route path='/manage/:id' element={<History />} />
         <Route path="/redirect/:option" element={<Redirect />} />
       </Routes>
     </div>
