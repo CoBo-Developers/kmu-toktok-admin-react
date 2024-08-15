849b78d51b7763055df07169e71a994e1a6a9af3
Jonghyeok <jhpmuscle@gmail.com>
2024-08-15 11:09:20 +0900

    feat: input states

--- src/components/aside/WritingMenu/CreateNewWriting.jsx
+++ src/components/aside/WritingMenu/CreateNewWriting.jsx
@@ -4,6 +4,54 @@ import { useState } from "react"
 
 function CreateNewWriting() {
   const [onCreate, setOnCreate] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [constraints, setConstraints] = useState("");
+  const [score, setScore] = useState();
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  }
+  
+  const handleDescriptionChange = (e) => {
+    handleTextareaChange(e);
+    setDescription(e.target.value);
+  }
+
+  const handleConstraintsChange = (e) => {
+    handleTextareaChange(e);
+    setConstraints(e.target.value);
+  }
+
+  const handleScoreChange = (e) => {
+    if (isNaN(e.target.value)) {
+      e.preventDefault();
+    } else {
+      setScore(e.target.value);
+    }
+  }
+
+  const handleButtonClick = () => {
+    if (title.trim().length <= 0) {
+      alert('제목을 입력해주세요.');
+      return ;
+    }
+
+    if (description.trim().length <= 0) {
+      alert('설명을 입력해주세요.');
+      return ;
+    }
+
+    if (!score || parseInt(score) < 0) {
+      alert('점수는 0점 이상이어야 합니다.');
+      return ;
+    }
+
+    if (title.trim().length <= 0) {
+      alert('제목을 입력해주세요.');
+      return ;
+    }
+  }
 
   return (
     <div>
@@ -12,21 +60,21 @@ function CreateNewWriting() {
           <section className="create-new-wrting">
             <article className="input">
               <label htmlFor="title">제목</label>
-              <input type="text" defaultValue={"1주차 추가과제"} placeholder="제목" />
+              <input type="text" placeholder="제목" onChange={handleTitleChange} />
             </article>
             <article className="textarea">
               <label htmlFor="description">설명</label>
               <textarea name="description" id="description" placeholder="설명" rows={1}
-              onChange={handleTextareaChange}></textarea>
+              onChange={handleDescriptionChange}></textarea>
             </article>
             <article className="textarea">
               <label htmlFor="constraints">규정</label>
-              <textarea name="constraints" id="constraints" placeholder="규정"
-              onChange={handleTextareaChange}></textarea>
+              <textarea name="constraints" id="constraints" placeholder="규정" rows={1}
+              onChange={handleConstraintsChange}></textarea>
             </article>
             <article className="input">
               <label htmlFor="score">총점</label>
-              <input type="text" name="score" id="score"/>
+              <input type="number" name="score" id="score" onChange={handleScoreChange} />
             </article>
             <article className="due-date-wrapper">
               <h5 htmlFor="due-date">기한</h5>
@@ -55,7 +103,7 @@ function CreateNewWriting() {
                 </div>
               </section>
             </article>
-            <button>
+            <button onClick={handleButtonClick}>
               확인
             </button>
           </section> : 
