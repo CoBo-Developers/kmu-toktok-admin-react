5f65f840b4752a69b9da05a227a1ab46117340e5
Jonghyeok <jhpmuscle@gmail.com>
2024-08-16 13:14:24 +0900

    feat: post writing api

--- src/api/writingApi.js
+++ src/api/writingApi.js
@@ -13,4 +13,22 @@ const getWritingList = async (token) => {
   return res.json();
 }
 
-export { getWritingList }
+const postWriting = async (data, token) => {
+  const res = await fetch(import.meta.env.VITE_APP_WRITING_API_URL + '/api/professor', {
+    method: 'POST',
+    headers: {
+      'authorization': 'Bearer ' + token,
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (!res.ok) {
+    const message = (await res.json()).message;
+    throw new Error(message);
+  }
+
+  return res.json();
+}
+
+export { getWritingList, postWriting }
