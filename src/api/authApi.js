5008541258541c6c7edbd0de81a76103f7f3b3f1
Jonghyeok <jhpmuscle@gmail.com>
2024-09-02 02:52:20 +0900

    fix: login api endpoint, authorization header

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,5 +1,5 @@
 const loginApi = async (code, option) => {
-    const res = await fetch(`${import.meta.env.VITE_APP_AUTH_API_URL}/api/auth/${option}-local-login?code=${decodeURIComponent(code)}`);
+    const res = await fetch(`${import.meta.env.VITE_APP_AUTH_API_URL}/api/auth/admin-${option}-login?code=${decodeURIComponent(code)}`);
 
     if (!res.ok) {
         const message = await res.json();
@@ -14,7 +14,7 @@ const reissueApi = async (refreshToken) => {
     const res = await fetch(`${import.meta.env.VITE_APP_AUTH_API_URL}/api/auth/login`, {
       method: 'PATCH',
       headers: {
-        Authorization: refreshToken,
+        Authorization: 'Bearer ' + refreshToken,
       },
     });
   
